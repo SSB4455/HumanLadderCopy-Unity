4f76a3e6282ee3c4df7ae2fdc0e014e8ceb849f4
SSB4455 <741086@gmal.com>
2015-03-06 10:48:31 +0800

    增加计数

--- Assets/Scripts/GroundControllerScript.cs
+++ Assets/Scripts/GroundControllerScript.cs
@@ -55,10 +55,8 @@ public class GroundControllerScript : MonoBehaviour
 					{
 						humans.Add(nextHumanTrans);
 					}
-					nextHumanTrans = (Instantiate(humanPrefab) as GameObject).transform;
+					nextHumanTrans = GetHuman();
 					nextHumanTrans.position = nextHumanPosition;
-					Rigidbody2D rigidbody2D = nextHumanTrans.GetComponent<Rigidbody2D>();
-					rigidbody2D.centerOfMass = new Vector2(0, 0.01f);
 				}
 				break;
 			case 1:		// 调整摄像机高度
@@ -96,19 +94,23 @@ public class GroundControllerScript : MonoBehaviour
 		for (int i = 0; i < humans.Count; i++)
 		{
 			maxHeight = humans[i].position.y > maxHeight ? humans[i].position.y : maxHeight;
+			if (humans[i].position.y < -4)
+			{
+				humans[i].gameObject.SetActive(false);
+				prepareHumans.Add(humans[i]);
+				humans.RemoveAt(i--);
+				ladderCount--;
+			}
 		}
 
 		if (maxHeight > cameraPositionY)
 		{
-			//cameraMoveShift = 0;
 			cameraPositionY = maxHeight + 1;
 		} else if (maxHeight < cameraPositionY - 2) {
-			//cameraMoveShift = 0;
 			cameraPositionY = maxHeight + 1;
 		}
 		if (mainCamera.transform.position.y != cameraPositionY)
 		{
-			Debug.Log(Mathf.Sign(cameraPositionY - mainCamera.transform.position.y));
 			float direction = Mathf.Sign(cameraPositionY - mainCamera.transform.position.y);
 			mainCamera.transform.Translate(0, direction * Time.deltaTime * cameraSpeed, 0);
 			if (direction * (cameraPositionY - mainCamera.transform.position.y) < 0)
@@ -126,73 +128,34 @@ public class GroundControllerScript : MonoBehaviour
 		}
 	}
 
-	/*void UpdateTouchs()
+	Transform GetHuman()
 	{
-		Touch[] currentTouches = Input.touches;
-		for (int i = 0; i < currentTouches.Length; i++)
+		Transform humanTrans = null;
+		if (prepareHumans.Count > 0)
 		{
-			bool inFingerTouchList = false;
-			for (int j = 0; j < fingerTouchList.Count; j++)
-			{
-				if (currentTouches[i].fingerId == fingerTouchList[j].touch.fingerId)
-				{
-					fingerTouchList[j].updated = true;
-					fingerTouchList[j].touch = currentTouches[i];
-					if (fingerTouchList[j].touch.tapCount > fingerTouchList[j].tapCount)
-					{
-						fingerTouchList[j].tapCount = fingerTouchList[j].touch.tapCount;
-						AddTouch(fingerTouchList[j]);
-					}
-					//Debug.Log("touch.tapCount = " + fingerTouchList[j].touch.tapCount);
-					inFingerTouchList = true;
-					break;
-				}
-			}
-
-			if (!inFingerTouchList)
-			{
-				if (fingerTouchPrepareList.Count < 1)
-				{
-					fingerTouchPrepareList.Add(new FingerTouch());
-				}
-				fingerTouchPrepareList[0].touch = currentTouches[i];
-				fingerTouchPrepareList[0].tapCount = currentTouches[i].tapCount;
-				fingerTouchPrepareList[0].updated = true;
-				fingerTouchPrepareList[0].onScreen = true;
-				FingerTouch fingerTouch = fingerTouchPrepareList[0];
-				fingerTouchList.Add(fingerTouch);
-				fingerTouchPrepareList.RemoveAt(0);
-				TouchNumber++;
-				AddTouch(fingerTouch);
-			}
-		}
-		for (int i = 0; i < fingerTouchList.Count; i++)
-		{
-			if (!fingerTouchList[i].updated || fingerTouchList[i].touch.phase == TouchPhase.Ended || fingerTouchList[i].touch.phase == TouchPhase.Canceled)
-			{
-				fingerTouchList[0].updated = false;
-				fingerTouchList[i].onScreen = false;
-				fingerTouchPrepareList.Add(fingerTouchList[i]);
-				fingerTouchList.RemoveAt(i);
-				TouchNumber--;
-				i--;
-			}
-			else
-			{
-				fingerTouchList[i].updated = false;
-			}
+			humanTrans = prepareHumans[0];
+			prepareHumans.RemoveAt(0);
+		} else {
+			humanTrans = (Instantiate(humanPrefab) as GameObject).transform;
+			Rigidbody2D rigidbody2D = nextHumanTrans.GetComponent<Rigidbody2D>();
+			rigidbody2D.centerOfMass = new Vector2(0, 0.01f);
 		}
 
-	}*/
-
+		ladderCount++;
+		humanTrans.gameObject.SetActive(true);
+		humanTrans.eulerAngles = Vector3.zero;
+		return humanTrans;
+	}
 
 	private GUIStyle fpsStyle;
 	private void OnGUI()
 	{
-		GUILayout.Label("Count = " + humans.Count
-			+ "Input.mousePosition = " + Input.mousePosition + "\nnextHumanPosition = " + nextHumanPosition
+		GUILayout.Label("ladderCount = " + ladderCount
+			//+ "\nInput.mousePosition = " + Input.mousePosition
+			+ "\nnextHumanPosition = " + nextHumanPosition
 			//+ "\nScreen.currentResolution.height/2 = " + (Screen.currentResolution.height / 2)
 			//+ "\nhalf screen Hight = " + mainCamera.ScreenPointToRay(new Vector3(0, Screen.currentResolution.height / 2, 0))
-			+ "\nScreenPointToRay = " + mainCamera.ScreenPointToRay(Input.mousePosition), fpsStyle);
+			//+ "\nScreenPointToRay = " + mainCamera.ScreenPointToRay(Input.mousePosition)
+			, fpsStyle);
 	}
 }
